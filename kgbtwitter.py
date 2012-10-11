903b3ac0928c8f986e593e30c6fb1fbcada4ae38
Peter van Dijk <peter.van.dijk@netherlabs.nl>
2012-10-11 20:36:28 +0200

    add password checking

--- kgbtwitter.py
+++ kgbtwitter.py
@@ -1,6 +1,7 @@
-from SOAPpy import SOAPServer
 import sys, os, json, urllib, datetime
+from SOAPpy import SOAPServer
 from twitter import Twitter, OAuth
+from hashlib import sha1
 
 def geturllength(url):
     twitterconfig = json.loads(
@@ -11,6 +12,19 @@ def geturllength(url):
         return twitterconfig['short_url_length_https']
     else:
         return twitterconfig['short_url_length']
+    
+def checkpassword(data, password):
+    message = ''.join([
+        data['repoid'],
+        data['rev'],
+        ''.join(data['paths']),
+        data['log'],
+        data['author'],
+        data['branch'] or '',
+        data['module'] or '',
+        password
+        ])
+    return sha1(message.encode('utf-8')).hexdigest() == data['checksum']
     
 def tweet(data, conf):
     body = conf['body'] % data
@@ -39,8 +53,13 @@ def tweet(data, conf):
 
 def commit(Array):
     data = dict(zip(('version', 'repoid', 'checksum', 'rev', 'paths', 'log', 'author', 'branch', 'module'), Array))
-    if data['repoid'] in config['repos']:
-        tweet(data, config['repos'][data['repoid']])
+    repoid = data['repoid']
+    if repoid in config['repos']:
+        if checkpassword(data, config['repos'][repoid]['password']):
+            tweet(data, config['repos'][data['repoid']])
+        else:
+            print '%s got wrong password!' % (datetime.datetime.now())
+            return "wrong password"
 
     return "OK"
 
